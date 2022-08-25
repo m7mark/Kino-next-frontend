1b441d9d56f69e7e05d980a6059d8c330ea82406
Mark <m7mark@yandex.ru>
2022-08-25 12:08:54 +0300

    Scroll to top

--- app/components/layout/Layout.tsx
+++ app/components/layout/Layout.tsx
@@ -1,15 +1,23 @@
-import { FC, ReactNode } from 'react'
+import { FC, ReactNode, useEffect, useRef } from 'react'
 
 import styles from './Layout.module.scss'
 import { Navigation } from './Navigation/Navigation'
 import { Sidebar } from './Sidebar/Sidebar'
 
 export const Layout: FC<{ children: ReactNode }> = ({ children }) => {
+  const ref = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    ref.current?.scrollTo(0, 0)
+  }, [children])
+
   return (
     <>
       <div className={styles.layout}>
         <Navigation />
-        <div className={styles.center}>{children}</div>
+        <div ref={ref} className={styles.center}>
+          {children}
+        </div>
         <Sidebar />
       </div>
     </>
